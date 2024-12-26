72bc1efa5f47a59ed458c7c60ea3726a542bcb0c
muntadher995 <mnr64118@gmail.com>
2024-12-26 11:39:58 +0300

    alialialia

--- NewFolder/Class.cs
+++ NewFolder/Class.cs
@@ -17,5 +17,18 @@ namespace WebApplication3.NewFolder
             })
             .ToArray();
         }
+
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
